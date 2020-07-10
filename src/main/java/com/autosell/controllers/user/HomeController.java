7275b14ea44a99d61878fa5d11835e917d10d835
sbr mgr <sbr.mgr1@gmail.com>
2020-07-10 17:59:47 -0500

    Managed Naming convension

--- src/main/java/com/autosell/controllers/user/HomeController.java
+++ src/main/java/com/autosell/controllers/user/HomeController.java
@@ -1,4 +1,4 @@
-package com.autosell.controllers.front;
+package com.autosell.controllers.user;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,6 +10,6 @@ public class HomeController {
 
     @GetMapping("/")
     public String home(){
-        return "front/index";
+        return "user/index";
     }
 }
