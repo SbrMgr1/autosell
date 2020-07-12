a32cc1fc8490e23cf3f7083e13dedd51c6bf7fd4
hailuhagos <hailuhh2019@gmail.com>
2020-07-12 00:20:09 -0500

    cms forms

---
+++ src/main/java/com/autosell/repositories/ContentRepository.java
@@ -0,0 +1,13 @@
+package com.autosell.repositories;
+
+import com.autosell.domains.Content;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ContentRepository extends CrudRepository<Content,String>{
+
+    @Query(value ="select * from Content c where c.slug = :slug", nativeQuery = true)
+    Content getContentBySlug(String slug);
+}
