78d7e4ecac332b39cf10a4e44850dfbb4abde5e0
emekwi <emekwi50@gmail.com>
2020-07-13 08:21:48 -0500

    commit

---
+++ src/main/java/com/autosell/controllers/user/couponController.java
@@ -0,0 +1,19 @@
+package com.autosell.controllers.user;
+
+import com.autosell.domains.Coupon;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class couponController {
+
+    @GetMapping(value = "/coupon")
+    public String viewCoupon(@ModelAttribute("couponDets") Coupon coupon, Model model){
+        return "user/couponDetails";
+    }
+
+}
