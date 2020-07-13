ac3ab48d2cd0a474e3f647b380e92d2c5eb70f40
pradipacharya7 <pradip.acharya077@gmail.com>
2020-07-13 18:14:02 -0500

    Test added

--- src/test/java/com/autosell/controllers/user/PageControllerTest.java
+++ src/test/java/com/autosell/controllers/user/PageControllerTest.java
@@ -8,16 +8,15 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 
 
-class PageControllerTest {
+public class PageControllerTest {
 
     @InjectMocks
     PageController pageController;
@@ -39,14 +38,7 @@ class PageControllerTest {
         String viewName = pageController.getPage("about", model);
         assertEquals("user/page", viewName);
         verify(contentService, times(1)).find("about");
-        verify(model, times(1)).addAttribute(eq("content"),argumentCaptor.capture());
+        verify(model, times(1)).addAttribute(eq("content"),any());
     }
 
-    @GetMapping("page/{slug}")
-    public String getPage(@PathVariable("slug") String slug, Model model) {
-        Content content = contentService.find(slug);
-        model.addAttribute("content", content);
-        return "user/page";
-
-    }
 }
